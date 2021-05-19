44a15f668fcf2737466c124fe034bfcd6f75571c
xfshahrukh44 <xixno1@gmail.com>
2021-05-20 04:33:52 +0500

    News | description collapse fixed

--- resources/views/admin/news/news.blade.php
+++ resources/views/admin/news/news.blade.php
@@ -595,6 +595,23 @@ $(document).ready(function(){
       $('#deleteNewsModal').modal('show');
     });
 
+    // on show-more click (see more collapse)
+    $(".show-more a").on("click", function() {
+      var $this = $(this); 
+      var $content = $this.parent().prev("div.content");
+      var linkText = $this.text().toUpperCase();    
+      
+      if(linkText === "SHOW MORE"){
+          linkText = "Show less";
+          $content.switchClass("hideContent", "showContent", 100);
+      } else {
+          linkText = "Show more";
+          $content.switchClass("showContent", "hideContent", 100);
+      };
+
+      $this.text(linkText);
+    });
+
     // on image click (preview)
     $('#editForm .image').on('change', function(){
       // console.log($(this)[0]);
